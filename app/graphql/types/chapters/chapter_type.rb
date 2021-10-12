7c422dde63a79ce3bba2e8c5849f965e89b193c6
k5953837 <k5953837@gmail.com>
2021-10-12 21:20:34 +0800

    Add chapter graphql type and connect with course type
    Signed-off-by: k5953837 <k5953837@gmail.com>

--- app/graphql/types/chapters/chapter_type.rb
+++ app/graphql/types/chapters/chapter_type.rb
@@ -1,11 +1,9 @@
 module Types
-  module Courses
-    class CourseType < Types::BaseObject
+  module Chapters
+    class ChapterType < Types::BaseObject
       field :id, GraphQL::Types::ID, null: false
       field :name, String, null: true
-      field :lecturer, String, null: true
-      field :description, String, null: true
-      field :chapters, [Types::Chapters::ChapterType], null: false
+      field :course, Types::Courses::CourseType, null: false
       field :created_at, GraphQL::Types::ISO8601DateTime, null: false
       field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
-# Create test course
+# Create test courses
 5.times do
   Course.create(
     name: Faker::Name.name,
     lecturer: Faker::Name.name,
     description: Faker::Lorem.paragraph
+  )
+end
+
+# Create test chapters
+10.times do
+  Chapter.create(
+    name: Faker::Name.name,
+    course_id: Course.ids.sample
   )
 end
